0c258dbea541cfa7d449153314bbfe1da5572a82
Ваше Имя <you@example.com>
2020-01-15 13:44:41 +1000

    ggg

---
+++ database_layer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 15.01.20
+ * Time: 13:42
+ */
+function format_text(){}
+
+function format_category(){}
+
+function fetch_articles(){}
+
+function fetch_categories(){}
