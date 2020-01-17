a48ab15b6e501e3f09d3bb54fc945b6a1e7dac50
Ваше Имя <you@example.com>
2020-01-17 13:50:27 +1000

    dd

---
+++ article_generator.php
@@ -0,0 +1,17 @@
+<?php
+require_once ("database_layer.php");
+
+function format_text($string_parameter)
+{
+    return "<p class='card-text'>$string_parameter</p>";
+}
+
+function format_article_card()
+{
+
+}
+
+function format_category()
+{
+
+}
--- database_layer.php
+++ database_layer.php
@@ -5,10 +5,23 @@
  * Date: 15.01.20
  * Time: 13:42
  */
-function format_text(){}
 
-function format_category(){}
+$link = mysqli_connect("192.168.200.79", "user", "user", "1131_vov");
 
-function fetch_articles(){}
+function fetch_articles($link)
+{
+    mysqli_query($link, 'SELECT * FROM table_article');
+}
 
-function fetch_categories(){}
+function fetch_categories($link)
+{
+    mysqli_query($link, 'SELECT * FROM table_category');
+}
+
+function fetch_article($id,$link)
+{
+
+    if(!isset($id))
+        return fetch_categories($link);
+    $result = mysqli_query($link, "SELECT * FROM table_article WHERE id = $id");
+}
