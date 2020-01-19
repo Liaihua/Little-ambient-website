8ae0b0016fc8e8d7f0cf552198c6ba17272f2135
Liaihua <ilyavovchenko431@gmail.com>
2020-01-19 22:50:53 +1000

    Задолбало

--- article_generator.php
+++ article_generator.php
@@ -6,7 +6,12 @@ function format_text($string_parameter)
     return "<p class='card-text'>$string_parameter</p>";
 }
 
-function format_article_card()
+function format_article_card($article) // для генерации превью статьи
+{
+
+}
+
+function format_article_full($article)
 {
 
 }
